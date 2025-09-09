fe4e05afeacde448ed589622aba02f1c8a78a1e4
umeh-promise <umehpromise1000@gmail.com>
2025-09-09 17:36:39 +0100

    Add QuizQuestion model and questions data for quiz functionality

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,12 @@
+class QuizQuestion {
+  final String question;
+  final List<String> answers;
+
+  const QuizQuestion(this.question, this.answers);
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
+}
