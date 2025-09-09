ea8b22d01923b70b06a232aeaa00529a8f71bf2b
umeh-promise <umehpromise1000@gmail.com>
2025-09-09 17:36:28 +0100

    Add AnswerButton widget for user interaction

---
+++ lib/answer_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  final String answerText;
+  final void Function() onPress;
+  const AnswerButton(this.answerText, {super.key, required this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPress,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromARGB(255, 49, 0, 147),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+      ),
+      child: Text(
+        answerText,
+        style: TextStyle(
+          fontSize: 14,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
